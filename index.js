c1c505dbe4e2de689123003db669497bc7c91c79
prodo56 <pradeept1856@gmail.com>
2016-11-21 20:04:54 -0800

    updated the bot to speak

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const token="EAAHtSVEcZCUEBAJWOGcaysKVcZB59bQboXZBhM6UZB3QU7Fs0wntxgbZBMqGAF7KyhS9evjtuY8cAczl8JtZBILtT3HP7qnCwFC7ToZAkfqT8d27xZAerB3UzaScfvSuSUJhEOmglTMBFVNLZBMpGplCevzaKaVkvv8OVUEj3SpqdcwZDZD"
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -18,13 +19,39 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
-    res.send('Error, wrong token')
+    res.sendStatus(200)
 })
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
